4c3e6d5d2e339e3d40f5a3ce0472d7ab7604c1de
David <grikurovdavid@gmail.com>
2024-07-22 12:05:35 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -236,14 +236,14 @@ function App() {
                             </div>
                         </div>
 
-                        <div className={'px-4 mt-4 flex justify-center'}>
+                        <div className={'px-4 mt-3 mb-1 flex justify-center'}>
                             <div className={'px-4 py-2 flex items-center space-x-2'}>
                                 <img src={dollarCoin} alt={'Dollar Coin'} className={'w-10 h-10'}/>
                                 <p className={'text-4xl text-white'}>{points.toLocaleString()}</p>
                             </div>
                         </div>
 
-                        <div className={'px-4 mt-4 flex justify-center'}>
+                        <div className={'px-4 mx-4 flex justify-center'}>
                             <div
                                 className={'w-80 h-80 p-4 rounded-full circle-outer'}
                                 onClick={handleCardClick}
