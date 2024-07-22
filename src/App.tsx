96440a740ee649dd14d484624c49d91be41ec459
David <grikurovdavid@gmail.com>
2024-07-22 12:17:14 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -207,7 +207,7 @@ function App() {
                 </div>
 
                 <div className={'flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0'}>
-                    <div className={'absolute h-full top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]'}>
+                    <div className={'absolute h-[95vh] top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]'}>
 
                         <div className={'px-4 mt-6 flex justify-between gap-2'}>
                             <div className={'bg-[#272a2f] rounded-lg px-4 py-2 w-full relative'}>
