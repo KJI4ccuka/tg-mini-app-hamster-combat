2bf9d592a503727792a7b4dd753670866ce3c1be
David <grikurovdavid@gmail.com>
2024-07-22 12:26:52 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
     ]
 
     const [levelIndex, setLevelIndex] = useState(0)
-    const [points, setPoints] = useState(989999999)
+    const [points, setPoints] = useState(59000000)
     const [clicks, setClicks] = useState<{ id: number, x: number, y: number } []>([])
     const pointsToAdd = 11
     const profitPerHour = 126420
@@ -187,7 +187,7 @@ function App() {
                             </div>
                         </div>
                         <div
-                            className={'flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-60'}>
+                            className={'flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64'}>
                             <img src={binanceLogo} alt='Exchange' className={'w-6 h-6'}/>
                             <div className={'flex-1 text-center'}>
                                 <p className={'text-xs text-[#85827d] font-medium'}>
